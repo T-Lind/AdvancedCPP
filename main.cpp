5add3dfc425aa1ce8684b5b85b17571e8b1331b8
T-Lind <tiernanxklindauer@gmail.com>
2022-09-03 15:15:39 -0500

    Strings in a function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cmath>
+
+void stringWork() {
+    using namespace std;
+    string myString = "Hello nerds!";
+
+    // Print the string itself
+    cout << myString << endl;
+
+    // Print the character at position 3
+    cout << myString[3] << endl;
+
+    // Print the length of the string
+    cout << myString.length() << endl;
+
+    // Find a substring or character inside the string and return the first index of it
+    cout << myString.find("nerd", 3) << endl;  // Second arg is where to start looking at
+
+    // Perform a substring operation - first arg is the starting location, second is the LENGTH
+    cout << myString.substr(2, 3) << endl;  // Prints 'llo'
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace std;
+    string myString = "Hello nerds!";
+    myString[6] = 't';
+
+    cout << myString << endl;
+    cout << myString.substr(2, 3) << endl;
+
+    cout << fmax(10.2, 3.3);
+
     return 0;
 }
